47bdb51dcce0a661c6fbeab387b87216617bb0c4
NubarNecefli <sd7lu5gj4@code.edu.az>
2023-11-16 11:56:00 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const div=document.getElementById("div");
+const btnss= document.querySelectorAll('.btn');
+
+
+let page = 1;
+const limit = 10;
+
+btnss.forEach(btn=>{
+    btn.addEventListener('click',() =>{
+        getProducts(btn.innerHTML)
+    });
+});
+
+
+
+
+async function getProducts (page) {
+
+    let skip = (page - 1) * limit
+
+    const response = await fetch(`https://dummyjson.com/products?page=${page}&limit=${limit}&skip=${skip}`)
+    const data = await response.json()
+    div.innerHTML="";
+    console.log(data);
+    
+    data.products.forEach(item => {
+        const p = document.createElement('p')
+        p.textContent = `${item.title}`
+        div.appendChild(p)
+    })
+
+    
+}
+
+getProducts()
